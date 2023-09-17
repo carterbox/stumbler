f4a856425f2cdc3b2d4fb134b26f1e0f343b393d
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-16 23:23:18 -0500

    NEW: Setup the project

---
+++ android/app/src/main/kotlin/com/example/mozumbler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mozumbler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/io/github/carterbox/mozumbler/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.carterbox.mozumbler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
